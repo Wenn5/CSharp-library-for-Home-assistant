3542817077704a25fa4701e3ba3683347cedfffe
Wenn <gugle.wenn@gmail.com>
2024-11-26 20:36:54 +0800

    Change default class name

--- ProgramTest.cs
+++ ProgramTest.cs
@@ -2,7 +2,7 @@
 
 namespace MyApp // Note: actual namespace depends on the project name.
 {
-    internal class Program
+    internal class ProgramTest
     {
         static void Main(string[] args)
         {
