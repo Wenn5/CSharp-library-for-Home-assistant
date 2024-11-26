d9d528a1316b4fc89c5a1aeda8eb7ef5a916c72b
Wenn <gugle.wenn@gmail.com>
2024-11-26 20:47:38 +0800

    Revert "Change default class name"
    This reverts commit 3542817077704a25fa4701e3ba3683347cedfffe.

--- Program.cs
+++ Program.cs
@@ -2,7 +2,7 @@
 
 namespace MyApp // Note: actual namespace depends on the project name.
 {
-    internal class ProgramTest
+    internal class Program
     {
         static void Main(string[] args)
         {
